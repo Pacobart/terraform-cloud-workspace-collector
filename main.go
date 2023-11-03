3c9b223b981c815d15e9d5751bc3d2bf573ad38f
Parker Barthlome <pbb@amazon.com>
2023-11-03 15:36:40 -0600

    adding support for variableset

--- main.go
+++ main.go
@@ -40,7 +40,9 @@ type Workspace struct {
 			} `json:"data"`
 		} `json:"project"`
 	} `json:"relationships"`
-	Variables []Variable
+	Variables    []Variable
+	VariableSets []VariableSet
+	Teams        []Team
 }
 
 type WorkspaceList struct {
@@ -71,6 +73,33 @@ type Variable struct {
 
 type VariableList struct {
 	Data  []Variable `json:"data"`
+	Links struct {
+		Next string `json:"next"`
+	} `json:"links"`
+}
+
+type VariableSet struct {
+	ID         string `json:"id"`
+	Attributes struct {
+		Name string `json:"name"`
+	} `json:"attributes"`
+}
+
+type VariableSetList struct {
+	Data  []VariableSet `json:"data"`
+	Links struct {
+		Next string `json:"next"`
+	} `json:"links"`
+}
+
+type Team struct {
+	Attributes struct {
+		Key string
+	}
+}
+
+type TeamList struct {
+	Data  []Team `json:"data"`
 	Links struct {
 		Next string `json:"next"`
 	} `json:"links"`
@@ -165,6 +194,36 @@ func getVariablesForWorkspace(baseUrl string, token string, organization string,
 	return allVariables
 }
 
+func getVariableSetsForWorkspace(baseUrl string, token string, organization string, workspaceID string) []VariableSet {
+	client := &http.Client{}
+
+	var allVariableSets []VariableSet
+	nextPageURL := fmt.Sprintf("%s/workspaces/%s/varsets", baseUrl, workspaceID)
+
+	for nextPageURL != "" {
+		req, err := http.NewRequest("GET", nextPageURL, nil)
+		check(err)
+
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+		req.Header.Add("Content-Type", "application/vnd.api+json")
+		resp, err := client.Do(req)
+		check(err)
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		check(err)
+
+		var variableSets VariableSetList
+		err = json.Unmarshal(body, &variableSets)
+		check(err)
+
+		allVariableSets = append(allVariableSets, variableSets.Data...)
+		nextPageURL = variableSets.Links.Next
+	}
+
+	return allVariableSets
+}
+
 func generateHCL(workspaces []Workspace) *hclwrite.File {
 	hclFile := hclwrite.NewEmptyFile()
 	rootBody := hclFile.Body()
@@ -176,23 +235,27 @@ func generateHCL(workspaces []Workspace) *hclwrite.File {
 		workspaceBlock := workspacesBody.AppendNewBlock(fmt.Sprintf("%s =", ws.Attributes.Name), nil)
 		workspaceBody := workspaceBlock.Body()
 
+		var variableSetName string
+		if ws.VariableSets != nil {
+			variableSetName = ws.VariableSets[0].Attributes.Name
+		}
+
 		workspaceBody.SetAttributeValue(ws.Attributes.Name, cty.ObjectVal(map[string]cty.Value{
 			"reponame":         cty.StringVal(ws.Attributes.VcsRepo.Identifier),
 			"description":      cty.StringVal(ws.Attributes.Description),
 			"branchname":       cty.StringVal(ws.Attributes.VcsRepo.Branch),
 			"agent":            cty.StringVal(ws.Relationships.AgentPool.Data.Id),
 			"project_id":       cty.StringVal(ws.Relationships.Project.Data.Id),
-			"teams":            cty.StringVal("NONE"),
-			"variableset_name": cty.StringVal("NONE"),
+			"variableset_name": cty.StringVal(variableSetName), // TODO: only supporting one for now
 		}))
 
-		//teamsBlock := workspaceBody.AppendNewBlock("teams =", nil)
-		//teamsBody := teamsBlock.Body()
-		//for _, team := range ws.Teams {
-		//	teamsBody.SetAttributeValue(variable.Attributes.Key, cty.ObjectVal(map[string]cty.Value{
-		//		"access":       cty.StringVal("somestring"),
-		//	}))
-		//}
+		teamsBlock := workspaceBody.AppendNewBlock("teams =", nil)
+		teamsBody := teamsBlock.Body()
+		for _, team := range ws.Teams {
+			teamsBody.SetAttributeValue(team.Attributes.Key, cty.ObjectVal(map[string]cty.Value{
+				"access": cty.StringVal("somestring"),
+			}))
+		}
 
 		variablesBlock := workspaceBody.AppendNewBlock("variables =", nil)
 		variablesBody := variablesBlock.Body()
@@ -212,6 +275,10 @@ func updateVariablesForWorkspace(ws *Workspace, variables []Variable) {
 	ws.Variables = variables
 }
 
+func updateVariableSetsForWorkspace(ws *Workspace, variableSets []VariableSet) {
+	ws.VariableSets = variableSets
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Error: Terraform organization name not provided")
@@ -226,6 +293,9 @@ func main() {
 		ws := &workspaces[i]
 		variables := getVariablesForWorkspace(BASEURL, apiToken, orgName, ws.Attributes.Name)
 		updateVariablesForWorkspace(ws, variables)
+
+		variableSets := getVariableSetsForWorkspace(BASEURL, apiToken, orgName, ws.ID)
+		updateVariableSetsForWorkspace(ws, variableSets)
 	}
 
 	// fmt.Println(fmt.Sprintf("%v workspaces found", len(workspaces)))
