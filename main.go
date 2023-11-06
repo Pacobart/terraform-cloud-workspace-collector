9fd79d03df1b345d81ad6e504aaa0e066d6e1156
Parker Barthlome <pbb@amazon.com>
2023-11-06 11:40:42 -0700

    moving hcl

--- main.go
+++ main.go
@@ -5,63 +5,15 @@ import (
 	"os"
 	"sync"
 
+	"github.com/Pacobart/terraform-cloud-workspace-collector/internal/hcl"
 	"github.com/Pacobart/terraform-cloud-workspace-collector/internal/helpers"
 	"github.com/Pacobart/terraform-cloud-workspace-collector/internal/tfteams"
 	"github.com/Pacobart/terraform-cloud-workspace-collector/internal/tfvariables"
 	"github.com/Pacobart/terraform-cloud-workspace-collector/internal/tfvariablesets"
 	"github.com/Pacobart/terraform-cloud-workspace-collector/internal/tfworkspaces"
-	"github.com/hashicorp/hcl/v2/hclwrite"
-	"github.com/zclconf/go-cty/cty"
 )
 
 var BASEURL = "https://app.terraform.io/api/v2"
-
-func generateHCL(workspaces []tfworkspaces.Workspace) *hclwrite.File {
-	hclFile := hclwrite.NewEmptyFile()
-	rootBody := hclFile.Body()
-
-	workspacesBlock := rootBody.AppendNewBlock("workspaces =", nil)
-	workspacesBody := workspacesBlock.Body()
-
-	for _, ws := range workspaces {
-		workspaceBlock := workspacesBody.AppendNewBlock(fmt.Sprintf("%s =", ws.Attributes.Name), nil)
-		workspaceBody := workspaceBlock.Body()
-
-		var variableSetName string
-		if ws.VariableSets != nil {
-			variableSetName = ws.VariableSets[0].Attributes.Name
-		}
-
-		workspaceBody.SetAttributeValue(ws.Attributes.Name, cty.ObjectVal(map[string]cty.Value{
-			"reponame":         cty.StringVal(ws.Attributes.VcsRepo.Identifier),
-			"description":      cty.StringVal(ws.Attributes.Description),
-			"branchname":       cty.StringVal(ws.Attributes.VcsRepo.Branch),
-			"agent":            cty.StringVal(ws.Relationships.AgentPool.Data.Id),
-			"project_id":       cty.StringVal(ws.Relationships.Project.Data.Id),
-			"variableset_name": cty.StringVal(variableSetName), // TODO: only supporting one for now
-		}))
-
-		teamsBlock := workspaceBody.AppendNewBlock("teams =", nil)
-		teamsBody := teamsBlock.Body()
-		for _, team := range ws.Teams {
-			teamsBody.SetAttributeValue(team.Relationships.Team.Data.Id, cty.ObjectVal(map[string]cty.Value{
-				"access": cty.StringVal(team.Attributes.Access),
-			}))
-		}
-
-		variablesBlock := workspaceBody.AppendNewBlock("variables =", nil)
-		variablesBody := variablesBlock.Body()
-		for _, variable := range ws.Variables {
-			variablesBody.SetAttributeValue(variable.Attributes.Key, cty.ObjectVal(map[string]cty.Value{
-				"value":       cty.StringVal(variable.Attributes.Value),
-				"category":    cty.StringVal(variable.Attributes.Category),
-				"description": cty.StringVal(variable.Attributes.Description),
-				"sensitive":   cty.BoolVal(variable.Attributes.Sensitive),
-			}))
-		}
-	}
-	return hclFile
-}
 
 func UpdateVariablesForWorkspace(ws *tfworkspaces.Workspace, variables []tfvariables.Variable) {
 	ws.Variables = variables
@@ -106,7 +58,7 @@ func main() {
 	// fmt.Println(fmt.Sprintf("%v workspaces found", len(workspaces)))
 
 	// Generate HCL file
-	hcl := generateHCL(workspaces)
+	hcl := hcl.GenerateHCL(workspaces)
 	tfFile, err := os.Create("workspaces.tfvars")
 	helpers.Check(err)
 	tfFile.Write(hcl.Bytes())
